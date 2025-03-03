574e20d1462f7ad8f77f7d22910ee0b2a0428bb3
Muzammil-Mz <Mujjuimran74@gmail.com>
2025-03-03 12:13:16 +0000

    working form

--- client/src/pages/public/Register.jsx
+++ client/src/pages/public/Register.jsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 const Register = () => {
-  const [formData, setformData] = {
+  const [formData, setFormData] = useState({
     fullName: "",
     email: "",
     password: "",
     age: "",
     phone: "",
+  });
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   async function handleSubmit(event) {
@@ -17,26 +21,20 @@ const Register = () => {
       let response = await axios.post(apiUrl, formData);
       console.log(response.data);
     } catch (error) {
-      console.log(error.response.data);
+      console.log(error.response?.data || "Error occurred");
     }
   }
 
-  const handleChange = (e) => {
-    setformData({ ...formData, [e.target.name]: e.target.value });
-  };
   return (
     <>
-      <div>Register</div>
+      <h2>Register</h2>
       <form onSubmit={handleSubmit}>
-        <input type="text " value={formData.fullName} placeholder="FullName" onChange={handleChange} />
-        <input type="email" value={formData.email} placeholder="Email" />
-        <input
-          type="password"
-          value={formData.password}
-          placeholder="password"
-        />
-        <input type="number" value={formData.age} placeholder="Age" />
-        <button type="submit">Register</button>
+        <input type="text" name="fullName" value={formData.fullName} placeholder="Full Name" onChange={handleChange} required />
+        <input type="email" name="email" value={formData.email} placeholder="Email" onChange={handleChange} required />
+        <input type="password" name="password" value={formData.password} placeholder="Password" onChange={handleChange} required />
+        <input type="number" name="age" value={formData.age} placeholder="Age" onChange={handleChange} required />
+        <input type="tel" name="phone" value={formData.phone} placeholder="Phone Number" onChange={handleChange} required />
+        <button type="submit" >Register</button>
       </form>
     </>
   );
